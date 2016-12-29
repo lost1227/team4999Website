78fc8c5f7966790e60926b6438dd63cbcbffa6ff
Jordan Powers <jordanpowers1227@gmail.com>
2016-12-29 14:15:47 -0800

    Request Check if logged in

--- query.php
+++ query.php
@@ -1,13 +1,15 @@
 <?php session_start(); ?>
 <?php
-$data = $_SESSION["DB"]->query("SELECT * FROM robots;");
-if($data->num_rows > 0){
-	while($row = $data->fetch_assoc()) {
-		echo('<div class="infoRow">');
-			echo('<p>Team: ' . $row["TeamNum"] . '</p>');
-			echo('<p>Drive System: ' . $row["DrvSys"] . '</p>');
+if ($_SESSION["loggedIn"]){
+	$data = $_SESSION["DB"]->query("SELECT * FROM robots;");
+	if($data->num_rows > 0){
+		while($row = $data->fetch_assoc()) {
+			echo('<div class="infoRow">');
+				echo('<p>Team: ' . $row["TeamNum"] . '</p>');
+				echo('<p>Drive System: ' . $row["DrvSys"] . '</p>');
+		}
+	} else {
+		echo('<p>No results!</p>');
 	}
-} else {
-	echo('<p>No results!</p>');
 }
 ?>
