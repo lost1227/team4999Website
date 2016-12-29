d60d4f494226b3ff80cc3b7e5a55ca40d41541e6
Jordan Powers <jordanpowers1227@gmail.com>
2016-12-29 14:12:17 -0800

    Added ajax requests

--- home.php
+++ home.php
@@ -15,22 +15,35 @@
 if (!$_SESSION["loggedIn"]) {
 	header( 'Location: https://frcteam4999.jordanpowers.net/login.php');
 	echo('#normal { display: none; }');
+	exit();
 }
 ?>
 </style>
+<script>
+function getSys() {
+	var request = new HMLHttpRequest();
+	reequest.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("container").innerHTML = this.responseText;
+		}
+	}
+	var loop = window.setTimeout(function() {
+		request.open(GET,query.php,true);
+	}, 30000);
+</script>
 </head>
-<body>
+<body onload="getSys()">
 <div id="normal">
 <h1 id="title">Scouting</h1>
 <div id="container">
-<div class="infoRow">
+<!--<div class="infoRow">
 	<p>Team: Team1</p>
 	<p>Drive System: sampleDriveSystem</p>
 </div>
 <div class="infoRow">
 	<p>Team: Team2</p>
 	<p>Drive System: sampleDriveSystem</p>
-</div>
+</div>-->
 </div>
 </div>
 <div id="noLogin" <?php if ($_SESSION["loggedIn"]) { echo('style="display: none;"'); } ?>>
---
+++ query.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<?php
+$data = $_SESSION["DB"]->query("SELECT * FROM robots;");
+if($data->num_rows > 0){
+	while($row = $data->fetch_assoc()) {
+		echo('<div class="infoRow">');
+			echo('<p>Team: ' . $row["TeamNum"] . '</p>');
+			echo('<p>Drive System: ' . $row["DrvSys"] . '</p>');
+	}
+} else {
+	echo('<p>No results!</p>');
+}
+?>
