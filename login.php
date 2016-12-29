fb7e2baa1d098e45a41c3d278d9c4bdbd520167d
Jordan Powers <jordanpowers1227@gmail.com>
2016-12-29 14:59:38 -0800

    Debugging

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ function clean($data) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$user = clean($_POST["user"]);
 	$pass = clean($_POST["pass"]);
-	$_SESSION["DB"] = new mysqli("localhost", $user, $pass, "frcteam4999");
+	$_SESSION["DB"] = new mysqli("localhost",$user,$pass,"frcteam4999");
 	$_SESSION["user"] = $user;
 	if (!$_SESSION["DB"]->connect_error) {
 		$_SESSION["loggedIn"] = True;
