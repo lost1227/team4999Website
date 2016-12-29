f2b5dad6022cbe33bbfa62a9cc0d7aa918f84a94
Jordan Powers <jordanpowers1227@gmail.com>
2016-12-29 12:52:01 -0800

    Php
    home.php will check the session to see if you are logged in

--- home.php
+++ home.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <html>
 <head>
 <title>Scouting website</title>
@@ -10,9 +11,16 @@
 	padding: 20px;
 	border-radius: 5px;
 }
+<?php
+if (!$_SESSION["loggedIn"]) {
+	header( 'Location: https://frcteam4999.jordanpowers.net/login.php');
+	echo('#normal { display: none; }');
+}
+?>
 </style>
 </head>
 <body>
+<div id="normal">
 <h1 id="title">Scouting</h1>
 <div id="container">
 <div class="infoRow">
@@ -23,6 +31,10 @@
 	<p>Team: Team2</p>
 	<p>Drive System: sampleDriveSystem</p>
 </div>
+</div>
+</div>
+<div id="noLogin" <?php if ($_SESSION["loggedIn"]) { echo('style="display: none;"'); } ?>>
+<p>You are not logged in. Please click <a href="https://frcteam4999.jordanpowers.net/login.php">here</a> if you are not redirected.</p>
 </div>
 </body>
 </html>
