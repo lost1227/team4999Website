6756379413efdc8a6b5e0c47366de5fa7e4fd808
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-01 09:44:07 -0800

    Sort results by team number

--- query.php
+++ query.php
@@ -2,7 +2,7 @@
 <?php
 if ($_SESSION["loggedIn"]){
 	$DB = new mysqli("localhost",$_SESSION["user"],$_SESSION["pass"],"frcteam4999");
-	$data = $DB->query("SELECT * FROM robots;");
+	$data = $DB->query("SELECT * FROM robots ORDER BY Team DESC;");
 	#echo($data->num_rows);
 	if($data->num_rows > 0){
 		while($row = $data->fetch_assoc()) {
