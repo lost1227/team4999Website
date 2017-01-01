700020712e4e99ec5e44bc4f6bf7fd0852f4619d
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-01 09:34:31 -0800

    Allow for a variable number of categories

--- query.php
+++ query.php
@@ -7,8 +7,9 @@ if ($_SESSION["loggedIn"]){
 	if($data->num_rows > 0){
 		while($row = $data->fetch_assoc()) {
 			echo('<div class="infoRow">');
-				echo('<p>Team: ' . $row["TeamNum"] . '</p>');
-				echo('<p>Drive System: ' . $row["DrvSys"] . '</p>');
+			foreach($row as $key => $value) {
+				echo('<p>'.$key.': '.$value.'</p>');
+			}
 			echo('</div>');
 		}
 	} else {
