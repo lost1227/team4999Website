a5ed64fce01b5a91860e5c469aa8711afa5724dc
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-01 09:45:00 -0800

    Sort the other way

--- query.php
+++ query.php
@@ -2,7 +2,7 @@
 <?php
 if ($_SESSION["loggedIn"]){
 	$DB = new mysqli("localhost",$_SESSION["user"],$_SESSION["pass"],"frcteam4999");
-	$data = $DB->query("SELECT * FROM robots ORDER BY Team DESC;");
+	$data = $DB->query("SELECT * FROM robots ORDER BY Team ASC;");
 	#echo($data->num_rows);
 	if($data->num_rows > 0){
 		while($row = $data->fetch_assoc()) {
