19202a0b6e472db8cc8f18076cdd45318766531d
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-07 17:09:20 -0800

    Removed duplicate title

--- info.php
+++ info.php
@@ -7,7 +7,6 @@
 <?php
 $DB = new mysqli("localhost",$_SESSION["user"],$_SESSION["pass"],"frcteam4999");
 $team = $_GET["team"];
-echo('<h1>'.$team.'</h1>');
 $data = $DB->query('SELECT * FROM robots WHERE Team = "'.$team.'";');
 	if($data->num_rows > 0){
 		while($row = $data->fetch_assoc()) {
