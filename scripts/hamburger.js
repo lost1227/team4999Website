36723869168a2553af0382038a2db3879ae5b3f0
Fabio Colacio <fizzcol@gmail.com>
2017-01-11 21:52:35 -0800

    further fixes to hamburger js

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,10 +1,8 @@
 //test to see if jquery is loading properly
 
-var toggleMenu = function(){
-	console.log("You just clicked the hamburger menu!");
-}
-
 $(function() {
     console.log( "ready!" );
-    $("#hamburger").click(toggleMenu());
+    $("#hamburger").click(function(){
+		console.log("You just clicked the hamburger menu!");
+	});
 });
