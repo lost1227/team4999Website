3aee98345c648cda353096e66e861b97e4ac7ccf
lost1227 <jordanpowers1227@gmail.com>
2017-01-11 20:03:11 -0800

    Fixed error
    $row is an associative array, it cannot be accessed by an index. Also, by putting the link outside of the <div> elements, the entire div will become clickable.
    
    Fixed the following error:
    [Wed Jan 11 19:50:54.664252 2017] [:error] [pid 2322] [client 192.168.1.211:37952] PHP Notice:  Array to string conversion in /var/www/frcteam4999.jordanpowers.net/public_html/query.php on line 19, referer: https://frcteam4999.jordanpowers.net/home.php
    [Wed Jan 11 19:50:54.664262 2017] [:error] [pid 2322] [client 192.168.1.211:37952] PHP Notice:  Undefined offset: -1 in /var/www/frcteam4999.jordanpowers.net/public_html/query.php on line 19, referer: https://frcteam4999.jordanpowers.net/home.php
    [Wed Jan 11 19:50:54.664275 2017] [:error] [pid 2322] [client 192.168.1.211:37952] PHP Warning:  count() expects at least 1 parameter, 0 given in /var/www/frcteam4999.jordanpowers.net/public_html/query.php on line 19, referer: https://frcteam4999.jordanpowers.net/home.php
    
    PHP error logs can be accessed at: https://frcteam4999.jordanpowers.net/logs/error.log

--- query.php
+++ query.php
@@ -9,18 +9,13 @@ $data = $DB->query("SELECT * FROM robots ORDER BY Team ASC;");
 #echo($data->num_rows);
 if($data->num_rows > 0){
 	while($row = $data->fetch_assoc()) {
+		echo('<a class="teamlink" href = /info.php?team='.str_replace(' ','_',$row["Team"]).'>');
 		echo('<div class="infoRow">');
 		foreach($row as $key => $value) {
 			$key = str_replace('_',' ',$key);
-			if($key == "Team"){
-				echo('<a class="teamlink" href = /info.php?team='.str_replace(' ','_',$value).'>');
-			}
 			echo('<p>'.$key.': '.$value.'</p>');
-			if($key == $row[$row.count() - 1]){
-				echo('</a>');
-			}
 		}
-		echo('</div>');
+		echo('</div></a>');
 	}
 } else {
 	echo('<p>No results!</p>');
