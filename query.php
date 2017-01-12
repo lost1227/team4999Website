e60953cd2f94fab08e60c829e1ce2d78a01ddbf0
Fabio Colacio <fizzcol@gmail.com>
2017-01-11 18:11:40 -0800

    fixed a terrible mistake

--- query.php
+++ query.php
@@ -9,18 +9,18 @@ $data = $DB->query("SELECT * FROM robots ORDER BY Team ASC;");
 #echo($data->num_rows);
 if($data->num_rows > 0){
 	while($row = $data->fetch_assoc()) {
+		echo('<div class="infoRow">');
 		foreach($row as $key => $value) {
 			$key = str_replace('_',' ',$key);
 			if($key == "Team"){
 				echo('<a class="teamlink" href = /info.php?team='.str_replace(' ','_',$value).'>');
-				echo('<div class="infoRow">');
 			}
 			echo('<p>'.$key.': '.$value.'</p>');
 			if($key == $row[$row.count() - 1]){
-				echo('</div>');
 				echo('</a>');
 			}
 		}
+		echo('</div>');
 	}
 } else {
 	echo('<p>No results!</p>');
