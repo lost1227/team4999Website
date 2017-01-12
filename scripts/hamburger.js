d8a3ec8bbc928724dc61ab677465e34838f2cef2
Fabio Colacio <fizzcol@gmail.com>
2017-01-11 22:07:16 -0800

    fixed selector again

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -5,7 +5,7 @@ $(function() {
     
     $("#hamburger").click(function(){
 		console.log("You just clicked the hamburger menu!");
-		$(this).slideToggle();
+		$("#hamburgermenu").slideToggle();
 	});
 	
 });
