3a30c86aea26a83927a62c5505b7df2a228d303f
Fabio Colacio <fizzcol@gmail.com>
2017-01-11 21:55:26 -0800

    hamburger menu should slide down when the button is clicked

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,7 +2,10 @@
 
 $(function() {
     console.log( "ready!" );
+    
     $("#hamburger").click(function(){
 		console.log("You just clicked the hamburger menu!");
+		$("hamburgermenu").slideToggle();
 	});
+	
 });
