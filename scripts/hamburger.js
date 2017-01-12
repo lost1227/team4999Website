b50f4361097ddd929db70a6c9e56f3a6b6f2fe8a
Fabio Colacio <fizzcol@gmail.com>
2017-01-11 21:49:21 -0800

    added test for hamburger click event

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,4 +1,10 @@
 //test to see if jquery is loading properly
+
+function toggleMenu(){
+	console.log("You just clicked the hamburger menu!");
+}
+
 $(function() {
     console.log( "ready!" );
+    $("#hamburger").click(toggleMenu());
 });
