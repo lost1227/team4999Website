fbb77db47addb544ce80d8b6a751401887d4b5fe
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-12 18:40:10 -0800

    linked wrong script

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Scouting website</title>
 		<link rel="stylesheet" href="style.css">
-		<script src="scripts/hamburger.js"></script>
+		<script src="scripts/updateList.js"></script>
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, user-scalable=0" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
