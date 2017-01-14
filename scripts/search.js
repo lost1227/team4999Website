21ba39a5968d1d19af3d1bd48966ce43541da3b4
Fabio Colacio <fizzcol@gmail.com>
2017-01-13 22:44:38 -0800

    empty search bar when closing it

--- scripts/search.js
+++ scripts/search.js
@@ -18,4 +18,5 @@ $("#search > input").on('input', function() {
 
 $("#closesearchbar").click(function(){
 	$("#search").slideUp();
+	$("#search > input").text(""); //empty contents of searchbar
 });
