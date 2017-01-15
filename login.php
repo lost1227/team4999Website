070d717a4762bc90b99dd86fd8111dcf7ba393c9
Fabio Colacio <fizzcol@gmail.com>
2017-01-14 20:08:40 -0800

    added text to the button

--- login.php
+++ login.php
@@ -53,7 +53,7 @@
 				echo('<input type="hidden" name="redirect" value="'.$redirect.'">');
 			}
 			?>
-			<div id="submitbtn" onclick="javascript:parentNode.submit();"></div>
+			<div id="submitbtn" onclick="javascript:parentNode.submit();">submit</div>
 		</form>
 	</body>
 </html>
