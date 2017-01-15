31a7ebbd82da0cb0093527c8b233fa61d6a96ecf
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-14 20:16:52 -0800

    wildcards on both sides

--- scripts/search.js
+++ scripts/search.js
@@ -16,7 +16,7 @@ $("#TeamSearch > input").on('input', function() {
 		clearFilters();
 	else
 		filter.enabled = true;
-		filter.Team = $("#TeamSearch > input").val();
+		filter.Team = "%" + $("#TeamSearch > input").val() + "%";
 	get();
 });
 
