c77ec141510781cf0f468e64895113537e41ec4c
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-20 20:53:36 -0800

    same

--- edit.php
+++ edit.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 #check if creating a new entry, or editing an existing entry
 #creates an associative array of the existing entry
 if(isset($_GET["team"])){
-	if(is_int($_GET["Team"]) {
+	if(is_int($_GET["Team"])) {
 		$data = $DB->query('SELECT * FROM robots WHERE Team = '.$_GET["Team"].';');
 		if($data->num_rows > 0){
 			$row = $data->fetch_assoc();
