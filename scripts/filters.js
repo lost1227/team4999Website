f2cdc6ba0b97f03d5dd794663882b9fa6bb23d03
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-22 12:18:11 -0800

    more filters

--- scripts/filters.js
+++ scripts/filters.js
@@ -11,18 +11,31 @@ $("#filterli").click(function() {
 });
 
 $("#Filters > input").on("change",function() {
+	var filterIDs = $("#mydiv span[id]").map(function() { return this.id; }).get(); // get ids of each filter element
+	for (var i = 0; i < filterIDs.length; i++) {
+		if($("#"+filterIDs[i]).checked) {
+			window.clearInterval(loop);
+			filter.enabled = true;
+		} else {
+			filter.enabled = false;
+		}
+	}
+	if(!filter.enabled) {
+		loop = window.setInterval(get(),5000);
+	}
 	if(this.checked) {
 		//alert(this.id);
 		if(this.id == "DriveSystemCheck") {
 			filter.Drive_System = $("#DriveSystemSelect").val();
 		} else {
-			filter[this.id] == 1;
+			filter[this.id.replace(/ /g,"_")] == 1;
 		}
 	} else {
 		if(this.id == "DriveSystemCheck") {
 			delete filter.Drive_System;
 		} else {
-			delete filter[this.id];
+			delete filter[this.id.replace(/ /g,"_")];
 		}
 	}
+	get();
 });
