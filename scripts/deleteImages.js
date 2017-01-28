f274009e4362d7e4bf9478f78498acab268a0aa0
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-28 14:34:42 -0800

    red border

--- scripts/deleteImages.js
+++ scripts/deleteImages.js
@@ -2,7 +2,9 @@ $(".deletePix").on("change",function() {
 	var imageId = this.value;
 	if(this.checked) {
 		document.getElementById(imageId).style.filter = "grayscale(100%)";
+		document.getElementById(imageId).style.border = "5px solid red";
 	} else {
 		document.getElementById(imageId).style.filter = "";
+		document.getElementById(imageId).style.border = "inherit";
 	}
 });
