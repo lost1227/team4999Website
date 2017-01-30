7a2cf0a7bd4757cc38a12592ffc6c5bd18d1bee0
Jordan Powers <jordanpowers1227@gmail.com>
2017-01-30 08:40:40 -0800

    Debugging null in SQL

--- functions.php
+++ functions.php
@@ -15,7 +15,9 @@ function formatAndQuery() { #first argument should be the query. %sv for strings
         $args[$key] = $DB->real_escape_string($val);
     }
 	$query  = vsprintf($query, $args);
-	str_replace("''","null",$query);
+	writeToLog("Old Query: ".$query,"query");
+	$query = str_replace('\'\'',"null",$query);
+	writeToLog("New Query: ".$query,"query");
     $result = $DB->query($query);
     if (!$result)
     {
